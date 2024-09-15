aace340b4ff9025aabc9be46824b3834ee42f3d0
Olyxz16 <cedric.sazos@tutanota.com>
2024-09-15 11:04:46 +0200

    Added homedir check to prompt

--- prompt.c
+++ prompt.c
@@ -17,11 +17,33 @@ int user(char **buf) {
     return len;
 }
 
+char *checkhomedir(int cwdlen, char *cwd) {
+    char *homedir = getenv("HOME");
+    if(homedir == NULL) {
+        return cwd;
+    }
+    if (strstr(cwd, homedir) == NULL) {
+        return cwd;
+    }
+    int homelen = strlen(homedir);
+    int remlen = cwdlen - homelen + 1;
+    char *rem = (char *)malloc(remlen * sizeof(char));
+    for(int i = 0 ; i < remlen ; i++) {
+        rem[i] = cwd[i+homelen];
+    }
+    char *res = (char *)malloc((remlen+1) * sizeof(char));
+    sprintf(res, "~%s", rem);
+    free(cwd);
+    free(rem);
+    return res;
+}
+
 void prompt() {
     char *cwdbuf;
     int cwdlen = cwd(&cwdbuf);
     char *userbuf;
     int userlen = user(&userbuf);
+    cwdbuf = checkhomedir(cwdlen, cwdbuf);
     char *prompt = malloc((cwdlen + userlen + 10) * sizeof(char));
     sprintf(prompt, "%s:%s> ", userbuf, cwdbuf);
     fputs(prompt, stderr);
