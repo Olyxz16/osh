55635565f38ed2aac5f73116a7efa18a7981e2a9
Olyxz16 <cedric.sazos@tutanota.com>
2024-09-19 01:15:16 +0200

    Started doing non-builtin command

--- exec.c
+++ exec.c
@@ -1,11 +1,14 @@
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
 #include "builtin.h"
 
 #define RUNNING -1
 #define TERMINATED 0
 #define ERROR 1
-
 
 
 int exec(int argc, char **argv) {
@@ -16,6 +19,15 @@ int exec(int argc, char **argv) {
         int status = execbuiltin(argc, argv);
         return status;
     }
-    // not builtin
+    
+    char *command = argv[0];
+    __pid_t child_pid = fork();
+    if(child_pid) { // we are parent
+        wait(NULL);
+    } else { // we are child
+        execvp(command, argv);
+        exit(0);
+    }
+
     return RUNNING;
 }
